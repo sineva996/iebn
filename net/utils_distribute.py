f87c1e6d1c31d421b5b0495bf528d18ef3811bbb
Tsin_brilliance <63459822+sineva996@users.noreply.github.com>
2023-04-24 20:38:04 +0800

    Update utils_distribute.py

--- net/utils_distribute.py
+++ net/utils_distribute.py
@@ -5,10 +5,8 @@ from torch import distributed as dist
 def get_rank():
     if not dist.is_available():
         return 0
-
     if not dist.is_initialized():
         return 0
-
     return dist.get_rank()
 
 def synchronize():
